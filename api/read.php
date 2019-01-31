924e76b07886fbd3912e8a411dc009efc53f05f5
viennaltran <lanvienhtran@gmail.com>
2019-01-30 23:48:04 -0800

    finished read operation on SGT for practice

---
+++ api/read.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once('mysql_connect.php');
+
+$query = "SELECT * FROM students";
+
+$result = mysqli_query($conn, $query);
+$output = [
+    'success' => false,
+    'errors' => [],
+    'data' => []
+];
+if(!empty($result)){
+    if(mysqli_num_rows( $result ) !==0 ){
+        $output['success'] = true;
+        while($row = mysqli_fetch_assoc($result)){
+          $output['data'][] = $row;
+        }
+    }
+}   
+
+$json_output= json_encode($output);
+print($json_output);
+
+?>
--- script.js
+++ script.js
@@ -4,41 +4,23 @@
 * 
 /**
  * Listen for the document to load and initialize the application
- */
+ */ 
 $(document).ready(initializeApp);
 
 /**
  * Define all global variables here.  
  */
 /***********************
- * itemArray - global array to hold item objects
+ * student_array - global array to hold student objects
  * @type {Array}
- * example of itemArray after input: 
- * itemArray = [
- *  { place:"Japan", amountSpent: "4.00", expenseCategory: "Food", description:"Takoyaki", id: "1", transactionDate: "2019-1-27" },
- *  { place:"Santa Barbara", amountSpent: "9.00", expenseCategory: "Goods", id: "2", description: "Dress", transactionDate: "2019-2-14" }
+ * example of student_array after input: 
+ * student_array = [
+ *  { name: 'Jake', course: 'Math', grade: 85 },
+ *  { name: 'Jill', course: 'Comp Sci', grade: 85 }
  * ];
- * today - global string to hold today date, month, year, etc.
- * @type {String}
- * dd - global string to hold today date of the month
- * @type {String}
- * mm - global string to hold today month
- * @type {String}
- * yyyy - global string to hold current year
- * @type {String}
- * todayDate - global string to hold today date in yyyy-mm-dd format
- * @type {String}
- * categories - global array to hold options for expense category
- * @type {Array}
  */
-var itemArray = [];
-var today = null;
-var dd = null;
-var mm = null;
-var monthInText = '';
-var yyyy = null;
-var todayDate = getTodayDate();
-var categories = ['Food','Souvenirs','Good','Accomodation','Transportation','Activity', 'Other']
+var student_array=[];
+// var gradeAverage=null;
 
 /***************************************************************************************************
 * initializeApp 
@@ -47,8 +29,9 @@ var categories = ['Food','Souvenirs','Good','Accomodation','Transportation','Act
 * initializes the application, including adding click handlers and pulling in any data from the server, in later versions
 */
 function initializeApp(){
-    addClickHandlersToElements();
+      addClickHandlersToElements();
 }
+
 /***************************************************************************************************
 * addClickHandlerstoElements
 * @params {undefined} 
@@ -56,7 +39,216 @@ function initializeApp(){
 *     
 */
 function addClickHandlersToElements(){
-    $("#addButton").click(handleAddClicked);
-    $("#cancelButton").click(handleCancelClick);
-    
+      $("#addButton").click(handleAddClicked);
+      $("#cancelButton").click(handleCancelClick);
+      // $("#serverButton").click(getStudentArray);
+      
 }
+
+/***************************************************************************************************
+ * handleAddClicked - Event Handler when user clicks the add button
+ * @param {object} event  The event object from the click
+ * @return: 
+       none
+ */
+function handleAddClicked(event){
+      addStudent();
+   
+}
+/***************************************************************************************************
+ * handleCancelClicked - Event Handler when user clicks the cancel button, should clear out student form
+ * @param: {undefined} none
+ * @returns: {undefined} none
+ * @calls: clearAddStudentFormInputs
+ */
+function handleCancelClick(){
+      clearAddStudentFormInputs();
+
+}
+/***************************************************************************************************
+ * addStudent - creates a student objects based on input fields in the form and adds the object to global student array
+ * @param {undefined} none
+ * @return undefined
+ * @calls clearAddStudentFormInputs, updateStudentList
+ */
+function addStudent(){
+
+      student={};
+      student.name=$("#studentName").val();
+      student.course=$("#course").val();
+      student.grade=$("#studentGrade").val();
+      createStudentAjax(student)
+      // student_array.push(student);
+
+      // updateStudentList(student_array);
+      // clearAddStudentFormInputs();
+      
+}
+/***************************************************************************************************
+ * clearAddStudentForm - clears out the form values based on inputIds variable
+ */
+function clearAddStudentFormInputs(){
+     
+      $("#studentName").val("");
+      $("#course").val("");
+      $("#studentGrade").val("");
+
+}
+/***************************************************************************************************
+ * renderStudentOnDom - take in a student object, create html elements from the values and then append the elements
+ * into the .student_list tbody
+ * @param {object} studentObj a single student object with course, name, and grade inside
+ */
+function renderStudentOnDom(studentObj){
+      var name=$("<td>").text(studentObj.name);
+      var course=$("<td>").text(studentObj.course);
+      var grade=$("<td>").text(studentObj.grade);
+      var id=studentObj.id;
+      console.log("id", id)
+      var deleteButton=$("<button>",{
+            class: 'btn btn-danger',
+            id:"deleteButton",
+            text:"Delete",
+            studentID:id,
+            click:function (){
+                  console.log("id", id)
+                  deleteStudentAjax(id);
+                  // $(newTR).remove();
+                  // var index=student_array.indexOf(studentObj);
+                  // student_array.splice(index,1);
+                  
+            }
+            
+
+      });
+      var operation=$("<td>").append(deleteButton);
+      var newTR=$("<tr>").addClass("delete")
+
+      var newData=$(newTR).append(name, course, grade, operation);
+      
+      $("tbody").append(newData);
+
+     
+      
+
+}
+
+/***************************************************************************************************
+ * updateStudentList - centralized function to update the average and call student list update
+ * @param students {array} the array of student objects
+ * @returns {undefined} none
+ * @calls renderStudentOnDom, calculateGradeAverage, renderGradeAverage
+ */
+function updateStudentList(studentArray){
+
+      $("tbody").text(" ");
+      for (var i = 0; i < studentArray.length; i++) {
+            console.log(studentArray[i]);
+            var student = studentArray[i];
+            renderStudentOnDom(student);
+        }
+      // calculateGradeAverage(studentArray);
+                                    //71
+      renderGradeAverage(calculateGradeAverage(studentArray));
+
+}
+/***************************************************************************************************
+ * calculateGradeAverage - loop through the global student array and calculate average grade and return that value
+ * @param: {array} students  the array of student objects
+ * @returns {number}
+ */
+function calculateGradeAverage(studentArray){
+      var total=0;
+      var gradeAverage=0;
+      for(i=0;i<studentArray.length;i++){ 
+          total= parseFloat(total)+parseFloat(studentArray[i].grade);
+          gradeAverage=Math.ceil(total/studentArray.length);
+      }
+
+      return gradeAverage
+           
+}
+/***************************************************************************************************
+ * renderGradeAverage - updates the on-page grade average
+ * @param: {number} average    the grade average
+ * @returns {undefined} none
+ */
+function renderGradeAverage(totalAverage){
+      $(".avgGrade").text(totalAverage);
+}
+
+
+function getDataFromServer(){
+var the_data={api_key:'Km6OOLjFBX'}
+
+$.ajax({
+      data:the_data,
+      dataType:'json',
+      method:'post',
+      url:'api/read.php',
+      success: function (response){
+            console.log(response);   
+            var server_array=response['data'];
+            updateStudentList(server_array);        
+      },
+      error: function (err) {
+            console.log(err);
+
+      }
+
+}); 
+}
+
+
+function createStudentAjax(student){
+      var the_data={
+            api_key:'Km6OOLjFBX',
+            name: student.name,
+            course: student.course,
+            grade:student.grade
+}
+      
+      $.ajax({
+            data:the_data,
+            dataType:'json',
+            method:'post',
+            url:'api/create.php',
+            success: function (response){
+                  student.id = response.new_id;
+                  getDataFromServer();
+                  clearAddStudentFormInputs();
+                  console.log(response);
+            },
+            error: function (err) {
+                  console.log(err);
+      
+            }
+      });
+      } 
+      
+
+function deleteStudentAjax(studentID){
+      console.log('studentID:',studentID);
+      var the_data={
+            api_key:'Km6OOLjFBX',
+            student_id:studentID
+}
+$.ajax({
+      data:the_data,
+      dataType:'json',
+      method:'post',
+      url:'https://s-apis.learningfuze.com/sgt/delete',
+      success: function (response){
+            getDataFromServer();
+            console.log(response);
+      },
+      error: function (err) {
+            console.log(err);
+
+      }
+      
+});
+}
+  
+// server dictates/control the information
+// this is still front-end (ajax) calling backend
